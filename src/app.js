65ef6bfb93da8ba5e7941405e9456bab5afe10fd
vuong le <vuonglqph14910@fpt.edu.vn>
2022-03-08 17:25:47 +0700

    upload

---
+++ src/app.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.myname = void 0;
+exports.myname = "lê quý vương";
+var myAge = 2;
+var status = true;
+var myObj = {};
+var numberArr = [1, 2, 3, 4];
+var object = {};
+var arrObject = [{ id: 1 }, { id: 2 }, { id: 3 }];
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+export
+const myname : string = "lê quý vương";
+const myAge : number = 2;
+const status : boolean = true;
+const myObj : object = {};
+const numberArr : number[] = [1,2,3,4];
+const object : object = {};
+const arrObject : object[] = [{id: 1}, {id: 2}, {id: 3}];
